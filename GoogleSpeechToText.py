e12bf18d7778445a9c78210456a222ed468019c2
Ali Osman Mollahüseyinoğlu <aliosmanmho@users.noreply.github.com>
2015-02-06 19:35:01 +0200

    GoogleSpeechtToText.py

---
+++ GoogleSpeechToText.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from subprocess import call
+import json
+
+print ("Start Listening...")
+
+rc = call("./myscript.sh",shell=True)
+
+with open('myscript.sh','rb') as file:
+    script = file.read()
+
+rc = call(script,shell=True)
+
+print ("finish")
+
+
+f = open('file.txt','r')
+
+contents = f.read()
+contents = contents.split('\n',1)[1]
+
+res = json.loads(contents)['result'][0]['alternative'][0]['transcript']
+
+print (res)
+
+f.close()
